6b5e19c3a615e680296f3dcedc822de0f683c10e
rajanmalakar <77271424+rajanmalakar@users.noreply.github.com>
2021-09-08 17:27:07 +0530

    my News-site
    this website is build from html ,css and bootstrap and back-end i use php

---
+++ admin/footer.php
@@ -0,0 +1,13 @@
+<!-- Footer -->
+<div id ="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <span> Submitted to Simardeep mam by <a href="">RAJAN,RAJ & UAJJAL</a></span>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /Footer -->
+</body>
+</html>
